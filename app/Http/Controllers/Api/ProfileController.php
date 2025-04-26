bd4712dba60d4cc64d2a7dd43489a9ee78db0b50
Kram-V <markanthonyvivar24@gmail.com>
2025-04-26 11:57:11 +0800

    added api for updating profile

---
+++ app/Http/Controllers/Api/ProfileController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ProfileController extends Controller
+{
+    public function update_profile(User $user, Request $request) {
+      if ($request->password) {
+        $request->validate([
+          'fullname' => 'required|string',
+          'username' => 'required|string',
+          'password' => 'string|confirmed',
+        ]);
+
+        $user->update([
+          'fullname' => $request->fullname,
+          'username' => $request->username,
+          'password' => Hash::make($request->password),
+        ]);
+
+      } else {
+        $request->validate([
+          'fullname' => 'required|string',
+          'username' => 'required|string'
+        ]);
+
+        $user->update([
+          'fullname' => $request->fullname,
+          'username' => $request->username,
+        ]);
+      }
+
+      return response()->json([
+        'message' => 'Your account updated successfully',
+        'user' => $user->fresh()->makeHidden(['password', 'token']),
+      ]);
+    }
+}
