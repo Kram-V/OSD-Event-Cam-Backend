ab1ee652d6fd156a8f7a541c9907892b0f78b1b5
Kram-V <markanthonyvivar24@gmail.com>
2025-05-04 09:16:50 +0800

    loaded relation with department

--- app/Http/Controllers/Api/ProgramController.php
+++ app/Http/Controllers/Api/ProgramController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 class ProgramController extends Controller
 {
     public function programs() {
-      $programs = Program::latest()->get();
+      $programs = Program::with('department')->latest()->get();
 
       return response()->json([
         'programs' => $programs,
